6f9949334764e2a1a5061f8d94d1598caef26a30
Ahmed Musawir <ahmed.musawir@gmail.com>
2015-08-23 14:39:35 +0800

    BootSass Workflow 2.0: withBourbon

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Diz Nutz</title>
+<?php include "header.php" ?>
 
-		<!-- Bootstrap CSS -->
-		<link href="style.css" rel="stylesheet">
-
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
-	<body>
+<section class="box">
+   BOURBON TESTING SUCCESS!
+</section>
 		<h1 class="text-center">Welcome To BootySass</h1>
 		<div class="jumbotron">
 			<div class="container">
@@ -56,9 +42,6 @@
 			</tbody>
 		</table>
 
-		<!-- jQuery -->
-		<script src="//code.jquery.com/jquery.js"></script>
-		<!-- Bootstrap JavaScript -->
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	</body>
-</html>
+<?php include "footer.php" ?>
+
+
