c2775d29867cb15408f0a4585a21206763c197c5
imbdev <moose@inspiredmommyboss.com>
2017-07-10 10:02:51 +0800

    Masonry Success - Button & Select - 10 July 2017

---
+++ index-2.php
@@ -0,0 +1,70 @@
+<?php include "header.php" ?>
+
+<section id="fp-block-2" class="">
+
+
+	<h2 class="row">
+		<i class="fa fa-quote-left fa-3x" aria-hidden="true"></i>
+
+		 I believe that each of us has a purpose in life. It is my passion to coach and empower you to find your greatness and purpose in life so you can live the life of your DREAMS!
+
+		<!-- <i class="fa fa-quote-right fa-3x pull-right" aria-hidden="true"></i> -->
+
+	</h2>
+
+
+	<div class="text-content clearfix">
+			
+		<h3>Are you feeling overwhelmed in your life, your job, your relationships?</h3>
+
+		<div class="row">
+			<img class="img-responsive pull-right col-sm-4 col-md-4 col-lg-4" src="http://www.cherylkasper.com/wp-content/uploads/2016/07/image-2.jpg" alt="">
+			<p class="col-sm-8 col-md-8 col-lg-8">
+				If so, this can create fears and anxiety that leave you in a state of overwhelm when you begin to think about stepping out of your comfort zone. You become detached and are not fully present in your life. I was there too, going through the motions every day like a robot. I am able to assist you in recognizing the areas of your life that are overwhelming you and help you to create a plan to break through them.
+			</p>
+		</div>
+		<h3>Do you feel stuck in the same habits that you wish to break? </h3>
+		<div class="row">
+			<img class="img-responsive pull-left col-sm-4 col-md-4 col-lg-4" src="http://www.cherylkasper.com/wp-content/uploads/2016/07/image-3.jpg" alt="">
+			<p class="col-sm-8 col-md-8 col-lg-8">
+				Repeating the same behaviors over and over is "insanity!" Is the negative self criticism in you mind saying,"you can't do this," or "you don't deserve this?" This is why you cannot break free of the chains that hold you back. I felt the same way. I had constant negative chatter in my mind that held me back from my goals for many years. Now I can help you recognize your repeating habits and behaviors that you do unconsciously each day and help you break that cycle.
+			</p>
+		</div>
+		<h3>Is anxiety or fear holding you back from making changes in your life that you have been putting off? </h3>
+
+		<div class="row">
+			<img class="img-responsive pull-right col-sm-4 col-md-4 col-lg-4" src="http://www.cherylkasper.com/wp-content/uploads/2016/07/image-5.jpg" alt="">
+			<p class="col-sm-8 col-md-8 col-lg-8">
+				 Are you afraid of failure? Success? Rejection? Fear and anxiety can be crippling! Do you break out in a sweat or begin to tremble at the thought of change? I have the proven skills that I used myself to help you ease these feelings and have you look forward to the change that you have only been thinking of.
+			</p>
+		</div>
+		<h3>Have you given up on your goals and dreams? </h3>
+		<div class="row">
+			<img class="img-responsive pull-left col-sm-4 col-md-4 col-lg-4" src="http://www.cherylkasper.com/wp-content/uploads/2016/07/image-4.jpg" alt="">
+			<p class="col-sm-8 col-md-8 col-lg-8">
+				 Do you have a vision of what your goals and dreams look like, but have a hard time putting them into motion? I can help you to nourish your body, mind and soul to grow into your authentic self and achieve your goals! I have broken through my feelings of overwhelm, feeling stuck, fears, and anxiety...and I am NOW Living MY DREAM! 
+			</p>
+		</div>
+	</div>
+
+	<div class="btn-block">
+		
+		<a href="/lifestyle-transformation/">
+			<img class=" size-medium wp-image-237 aligncenter center-block" src="http://www.cherylkasper.com/wp-content/uploads/2015/06/healthy-inoutbig-300x200.png" alt="healthy inoutbig" width="300" height="200" />
+		</a>
+
+		<h2>
+			<a class="btn btn-primary btn-lg" href="/lifestyle-transformation/" title="">Keep Reading</a>
+			<br>
+			<span>... and I'll explain how I changed my life and how I can assist you in changing yours!!!	</span>
+		</h2>	
+
+	</div>
+
+	
+</section>
+
+
+<?php include "footer.php" ?>
+
+
